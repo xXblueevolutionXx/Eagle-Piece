e63876a5adc05eb2ccdfdd74fb815e95c2bd37a0
xXBLUE_EVOLUTIONXx <blueevolutionxred@gmail.com>
2026-03-14 00:11:50 -0500

    Update Main.java

--- src/main/java/me/yourname/onepiece/Main.java
+++ src/main/java/me/yourname/onepiece/Main.java
@@ -1,26 +1,35 @@
 package me.yourname.onepiece;
 
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.DamageSource;
+import net.minecraft.block.material.Material;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 
-@Mod(modid = "eaglepiece", name = "Eagle Piece", version = "1.0")
-public class Main {
+public class DevilFruit {
+    private String name;
 
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-        // This creates your fruit object
-        DevilFruit rubberFruit = new DevilFruit("Gomu Gomu");
-        
-        // This starts your Haki and Commands systems
-        Haki playerHaki = new Haki();
-        Commands gameCommands = new Commands();
+    public DevilFruit(String name) {
+        this.name = name;
+    }
 
-        // This gives the player the jump power as soon as the mod loads
-        if (Minecraft.getMinecraft().thePlayer != null) {
-            rubberFruit.applyPower(Minecraft.getMinecraft().thePlayer);
+    // Gives the rubber powers
+    public void applyPower(EntityPlayer player) {
+        if (this.name.equalsIgnoreCase("Gomu Gomu")) {
+            // Potion ID 8 is Jump Boost
+            player.addPotionEffect(new PotionEffect(Potion.jump.id, 200, 1));
         }
+    }
 
-        System.out.println("Eagle-Piece: Systems Online!");
+    // The "Curse of the Sea" logic
+    public void checkWaterWeakness(EntityPlayer player) {
+        if (player.isInsideOfMaterial(Material.water)) {
+            // Deals 1 point (half heart) of damage
+            player.attackEntityFrom(DamageSource.drown, 1.0F);
+            
+            // Slows the player down significantly
+            player.motionX *= 0.1;
+            player.motionZ *= 0.1;
+        }
     }
 }
