122810580accc656281e3403a7e0f9396f5fac16
xXBLUE_EVOLUTIONXx <blueevolutionxred@gmail.com>
2026-03-14 00:24:25 -0500

    Update Commands.java

--- src/main/java/me/yourname/onepiece/Commands.java
+++ src/main/java/me/yourname/onepiece/Commands.java
@@ -4,15 +4,15 @@ import net.minecraft.entity.player.EntityPlayer;
 
 public class Commands {
     
-    public void handleCommand(String command, EntityPlayer player, Haki hakiSystem) {
-        if (command.equalsIgnoreCase("/haki on")) {
-            hakiSystem.toggleArmament(player);
+    public void handleCommand(String command, EntityPlayer player, Haki haki, Factions factions) {
+        if (command.equalsIgnoreCase("/join marines")) {
+            factions.setSide(Factions.Side.MARINE, player);
         } 
-        else if (command.equalsIgnoreCase("/haki conqueror")) {
-            hakiSystem.useConquerors(player);
+        else if (command.equalsIgnoreCase("/join pirates")) {
+            factions.setSide(Factions.Side.PIRATE, player);
         }
-        else if (command.equalsIgnoreCase("/fruit info")) {
-            System.out.println("Fruit: Gomu Gomu no Mi");
+        else if (command.equalsIgnoreCase("/haki on")) {
+            haki.toggleArmament(player);
         }
     }
 }
