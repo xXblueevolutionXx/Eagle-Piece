09b211cec86c3f317372d9a5a2b12e7da4ab3107
xXBLUE_EVOLUTIONXx <blueevolutionxred@gmail.com>
2026-03-14 10:31:35 -0500

    Update Factions.java

--- src/main/java/me/yourname/onepiece/Factions.java
+++ src/main/java/me/yourname/onepiece/Factions.java
@@ -1,25 +1,41 @@
 package me.yourname.onepiece;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 
 public class Factions {
-    public enum Side { PIRATE, MARINE, NONE }
-    
-    private Side playerSide = Side.NONE;
 
-    public void setSide(Side side, EntityPlayer player) {
-        this.playerSide = side;
-        System.out.println("You are now a " + side.name() + "!");
+    public enum Side {
+        NEUTRAL, PIRATE, MARINE
     }
 
+    private Side currentSide = Side.NEUTRAL;
+    private int berries = 0; // Your new currency/bounty
+
     public Side getSide() {
-        return this.playerSide;
+        return currentSide;
+    }
+
+    public int getBerries() {
+        return berries;
+    }
+
+    // This updates your money
+    public void addBerries(int amount) {
+        this.berries += amount;
+    }
+
+    public void setSide(Side side, EntityPlayer player) {
+        this.currentSide = side;
+        player.addChatMessage(new ChatComponentText("You are now a " + side.toString() + "!"));
     }
 
     public void applyFactionBuffs(EntityPlayer player) {
-        if (playerSide == Side.MARINE) {
-            // Marines get a speed boost (Soru)
-            player.addPotionEffect(new net.minecraft.potion.PotionEffect(1, 200, 1));
+        // Marines get Soru (Speed) automatically
+        if (currentSide == Side.MARINE) {
+            player.addPotionEffect(new PotionEffect(Potion.moveSpeed.id, 20, 1));
         }
     }
 }
