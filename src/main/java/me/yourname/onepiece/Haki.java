a8f0d4f79dfbf843b3a721e7dc0769698136a005
xXBLUE_EVOLUTIONXx <blueevolutionxred@gmail.com>
2026-03-14 00:13:27 -0500

    Update Haki.java

--- src/main/java/me/yourname/onepiece/Haki.java
+++ src/main/java/me/yourname/onepiece/Haki.java
@@ -1,35 +1,48 @@
 package me.yourname.onepiece;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
+import java.util.List;
 
 public class Haki {
-    private int level;
-    private boolean isArmamentActive;
+    private int level = 1;
+    private boolean isArmamentActive = false;
 
-    public Haki() {
-        this.level = 1;
-        this.isArmamentActive = false;
-    }
-
-    // This activates the physical "Armor" effect
     public void toggleArmament(EntityPlayer player) {
         this.isArmamentActive = !this.isArmamentActive;
-        
         if (isArmamentActive) {
-            // Resistance II (ID 11) and Strength II (ID 5)
             player.addPotionEffect(new PotionEffect(Potion.resistance.id, 1000, 1));
             player.addPotionEffect(new PotionEffect(Potion.damageBoost.id, 1000, 1));
-            System.out.println("Busoshoku Haki: Hardened!");
         } else {
             player.removePotionEffect(Potion.resistance.id);
             player.removePotionEffect(Potion.damageBoost.id);
         }
     }
 
-    public void train() {
-        this.level++;
-        System.out.println("Haki Level Up! Current Level: " + this.level);
+    // NEW: The Conqueror's Blast
+    public void useConquerors(EntityPlayer player) {
+        System.out.println("HAOSHOKU HAKI ACTIVATED!");
+        
+        // Create a box 10 blocks around the player
+        double range = 10.0D;
+        AxisAlignedBB area = player.getEntityBoundingBox().expand(range, 5.0D, range);
+        
+        // Find all living things in that box
+        List<EntityLivingBase> targets = player.worldObj.getEntitiesWithinAABB(EntityLivingBase.class, area);
+
+        for (EntityLivingBase target : targets) {
+            if (target != player) {
+                // Blast them away!
+                double varX = target.posX - player.posX;
+                double varZ = target.posZ - player.posZ;
+                target.knockBack(player, 5.0F, varX, varZ);
+                
+                // Give them Weakness (ID 18) because they are "fainting"
+                target.addPotionEffect(new PotionEffect(Potion.weakness.id, 200, 2));
+            }
+        }
     }
 }
