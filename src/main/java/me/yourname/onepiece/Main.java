fa207efbe825dba1d0044e566706dd1194a37626
xXBLUE_EVOLUTIONXx <blueevolutionxred@gmail.com>
2026-03-14 10:17:07 -0500

    Update Main.java

--- src/main/java/me/yourname/onepiece/Main.java
+++ src/main/java/me/yourname/onepiece/Main.java
@@ -1,46 +1,14 @@
-package me.yourname.onepiece;
-
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraftforge.common.MinecraftForge;
-
-@Mod(modid = "eaglepiece", name = "Eagle Piece", version = "1.0")
-public class Main {
-    
-    // This 'instance' allows other files to talk to Main easily
-    public static Main instance;
-
-    // Initialize all your systems
-    public DevilFruit myFruit = new DevilFruit("Gomu Gomu");
-    public Haki myHaki = new Haki();
-    public Commands myCommands = new Commands();
-    public Factions myFactions = new Factions();
-    public MobManager myMobs = new MobManager();
-
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-        instance = this;
-
-        // Register everything to the Event Bus so the code actually runs
-        MinecraftForge.EVENT_BUS.register(this);
-        MinecraftForge.EVENT_BUS.register(myMobs); 
-        
-        System.out.println("Eagle-Piece: All systems connected!");
-    }
-
-    @SubscribeEvent
-    public void onPlayerTick(TickEvent.PlayerTickEvent event) {
-        // 1. If you are a Pirate, you get Fruit powers but drown in water
-        if (myFactions.getSide() == Factions.Side.PIRATE) {
-            myFruit.applyPower(event.player);
-            myFruit.checkWaterWeakness(event.player);
-        } 
-        
-        // 2. If you are a Marine, you get Soru (Speed)
-        else if (myFactions.getSide() == Factions.Side.MARINE) {
-            myFactions.applyFactionBuffs(event.player);
+@SubscribeEvent
+public void onBlockBreak(BlockEvent.BreakEvent event) {
+    // 10% chance when breaking leaves
+    if (event.state.getBlock() instanceof net.minecraft.block.BlockLeaves) {
+        if (myFruit.rollTreeSpawn()) {
+            // This drops the "Apple" named Gomu Gomu no Mi
+            net.minecraft.entity.item.EntityItem drop = new net.minecraft.entity.item.EntityItem(
+                event.world, event.pos.getX(), event.pos.getY(), event.pos.getZ(), 
+                myFruit.getFruitItem("Gomu Gomu no Mi")
+            );
+            event.world.spawnEntityInWorld(drop);
         }
     }
 }
