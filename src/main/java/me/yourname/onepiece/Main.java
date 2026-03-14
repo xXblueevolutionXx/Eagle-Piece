bd55822b6ca079a20ac4002cf6036c1b2644b9b8
xXBLUE_EVOLUTIONXx <blueevolutionxred@gmail.com>
2026-03-14 10:32:18 -0500

    Update Main.java

--- src/main/java/me/yourname/onepiece/Main.java
+++ src/main/java/me/yourname/onepiece/Main.java
@@ -6,10 +6,14 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraft.block.BlockLeaves;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.monster.EntitySkeleton;
 
 @Mod(modid = "eaglepiece", name = "Eagle Piece", version = "1.0")
 public class Main {
@@ -21,24 +25,45 @@ public class Main {
     public Commands myCommands = new Commands();
     public Factions myFactions = new Factions();
     public MobManager myMobs = new MobManager();
-    public KeyInputHandler myKeys; // New Key Handler
+    public KeyInputHandler myKeys;
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         instance = this;
 
-        // Register Keybinds ONLY on the client (the player's game)
+        // Client-side only setup (HUD and Keys)
         if (event.getSide().isClient()) {
             myKeys = new KeyInputHandler();
             MinecraftForge.EVENT_BUS.register(myKeys);
+            
+            OnepieceHUD myHUD = new OnepieceHUD();
+            MinecraftForge.EVENT_BUS.register(myHUD);
         }
 
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(myMobs); 
         
-        System.out.println("Eagle-Piece: Keybinds and Mobs Loaded!");
+        System.out.println("Eagle-Piece: All Systems Online!");
     }
 
+    // 1. Berry Reward System
+    @SubscribeEvent
+    public void onEntityKill(LivingDeathEvent event) {
+        if (event.source.getEntity() instanceof EntityPlayer) {
+            Factions.Side side = myFactions.getSide();
+            
+            // Pirates earn Berries for killing Marines (Zombies)
+            if (side == Factions.Side.PIRATE && event.entityLiving instanceof EntityZombie) {
+                myFactions.addBerries(100);
+            } 
+            // Marines earn Berries for killing Pirates (Skeletons)
+            else if (side == Factions.Side.MARINE && event.entityLiving instanceof EntitySkeleton) {
+                myFactions.addBerries(100);
+            }
+        }
+    }
+
+    // 2. Fruit Spawning Logic
     @SubscribeEvent
     public void onBlockBreak(BlockEvent.BreakEvent event) {
         if (event.state.getBlock() instanceof BlockLeaves) {
@@ -62,14 +87,10 @@ public class Main {
         }
     }
 
+    // 3. Power and Faction Updates
     @SubscribeEvent
     public void onPlayerTick(TickEvent.PlayerTickEvent event) {
         if (myFactions.getSide() == Factions.Side.PIRATE) {
             myFruit.applyPower(event.player);
             myFruit.checkWaterWeakness(event.player);
-        } else if (myFactions.getSide() == Factions.Side.MARINE) {
-            myFactions.applyFactionBuffs(event.player);
-        }
-
-    }
-}
+        } else if (myFactions.getSide() == F
