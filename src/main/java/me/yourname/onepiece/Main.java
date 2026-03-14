e6cbec06ebcbedf3a39057158714d284e960997f
xXBLUE_EVOLUTIONXx <blueevolutionxred@gmail.com>
2026-03-14 00:08:56 -0500

    Update Main.java

--- src/main/java/me/yourname/onepiece/Main.java
+++ src/main/java/me/yourname/onepiece/Main.java
@@ -1,26 +1,21 @@
 package me.yourname.onepiece;
 
-// These lines tell the game where to find Minecraft's tools
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraft.client.Minecraft; // Added to find the player
 
-// This @Mod line is CRITICAL. It registers your mod in the game.
 @Mod(modid = "eaglepiece", name = "Eagle Piece", version = "1.0")
 public class Main {
 
-    // This is the "Init" method. It replaces the "main" method for mods.
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        // Now we link your other files
-        Haki playerHaki = new Haki();
-        Commands gameCommands = new Commands();
-        DevilFruit rubberFruit = new DevilFruit("Gomu Gomu", "Paramecia");
-
-        // These messages will now show up in the Game Console/Log
-        System.out.println("Eagle-Piece: Mod Loaded!");
+        DevilFruit rubberFruit = new DevilFruit("Gomu Gomu");
         
-        rubberFruit.eat();
-        playerHaki.train();
-        gameCommands.handleCommand("/haki on");
+        // This gets the person currently playing the game
+        if (Minecraft.getMinecraft().thePlayer != null) {
+            rubberFruit.applyPower(Minecraft.getMinecraft().thePlayer);
+        }
+        
+        System.out.println("Eagle-Piece: Powers Loaded!");
     }
 }
