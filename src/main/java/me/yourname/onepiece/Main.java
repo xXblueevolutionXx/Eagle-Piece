0ad14706ac528362e50fa2f67694e92955ecd89b
xXBLUE_EVOLUTIONXx <blueevolutionxred@gmail.com>
2026-03-14 00:09:31 -0500

    Update Main.java

--- src/main/java/me/yourname/onepiece/Main.java
+++ src/main/java/me/yourname/onepiece/Main.java
@@ -2,20 +2,25 @@ package me.yourname.onepiece;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraft.client.Minecraft; // Added to find the player
+import net.minecraft.client.Minecraft;
 
 @Mod(modid = "eaglepiece", name = "Eagle Piece", version = "1.0")
 public class Main {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
+        // This creates your fruit object
         DevilFruit rubberFruit = new DevilFruit("Gomu Gomu");
         
-        // This gets the person currently playing the game
+        // This starts your Haki and Commands systems
+        Haki playerHaki = new Haki();
+        Commands gameCommands = new Commands();
+
+        // This gives the player the jump power as soon as the mod loads
         if (Minecraft.getMinecraft().thePlayer != null) {
             rubberFruit.applyPower(Minecraft.getMinecraft().thePlayer);
         }
-        
-        System.out.println("Eagle-Piece: Powers Loaded!");
+
+        System.out.println("Eagle-Piece: Systems Online!");
     }
 }
