3ca868e94451db6043f0616a76f7a948ff52ebc7
xXBLUE_EVOLUTIONXx <blueevolutionxred@gmail.com>
2026-03-14 00:12:47 -0500

    Update Haki.java

--- src/main/java/me/yourname/onepiece/Haki.java
+++ src/main/java/me/yourname/onepiece/Haki.java
@@ -1,25 +1,35 @@
 package me.yourname.onepiece;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+
 public class Haki {
     private int level;
-    private boolean isConquerorsUnlocked;
+    private boolean isArmamentActive;
 
     public Haki() {
         this.level = 1;
-        this.isConquerorsUnlocked = false;
+        this.isArmamentActive = false;
+    }
+
+    // This activates the physical "Armor" effect
+    public void toggleArmament(EntityPlayer player) {
+        this.isArmamentActive = !this.isArmamentActive;
+        
+        if (isArmamentActive) {
+            // Resistance II (ID 11) and Strength II (ID 5)
+            player.addPotionEffect(new PotionEffect(Potion.resistance.id, 1000, 1));
+            player.addPotionEffect(new PotionEffect(Potion.damageBoost.id, 1000, 1));
+            System.out.println("Busoshoku Haki: Hardened!");
+        } else {
+            player.removePotionEffect(Potion.resistance.id);
+            player.removePotionEffect(Potion.damageBoost.id);
+        }
     }
 
     public void train() {
         this.level++;
-        System.out.println("Your Haki is getting stronger! Current Level: " + level);
-        
-        if (level >= 50 && !isConquerorsUnlocked) {
-            isConquerorsUnlocked = true;
-            System.out.println("YOU HAVE AWAKENED CONQUEROR'S HAKI!");
-        }
-    }
-
-    public void useArmament() {
-        System.out.println("Armament Haki activated! Defense and Attack boosted.");
+        System.out.println("Haki Level Up! Current Level: " + this.level);
     }
 }
