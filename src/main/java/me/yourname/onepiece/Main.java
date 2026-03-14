dcf523a98b2a56d5a31f43ead19bb2884e161015
xXBLUE_EVOLUTIONXx <blueevolutionxred@gmail.com>
2026-03-14 00:00:18 -0500

    Update Main.java

--- src/main/java/me/yourname/onepiece/Main.java
+++ src/main/java/me/yourname/onepiece/Main.java
@@ -2,10 +2,15 @@ package me.yourname.onepiece;
 
 public class Main {
     public static void main(String[] args) {
-        // Creating the Gomu Gomu no Mi
+        // 1. Give the player a fruit
         DevilFruit luffyFruit = new DevilFruit("Gomu Gomu no Mi", "Paramecia");
-        
-        // Simulating the player eating it
         luffyFruit.eat();
+
+        // 2. Initialize Haki
+        Haki playerHaki = new Haki();
+        
+        // 3. Simulate training
+        playerHaki.useArmament();
+        playerHaki.train();
     }
 }
